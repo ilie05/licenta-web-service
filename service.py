200adc6e183749394f21e1d4e800c6e7a191158f
yoki <petras.moil@gmail.com>
2019-05-09 18:26:44 +0300

    new ip addr for mongo

--- service.py
+++ service.py
@@ -1,6 +1,6 @@
 import pymongo
 
-client = pymongo.MongoClient('mongodb://192.168.56.1:27017/')
+client = pymongo.MongoClient('mongodb://192.168.56.255:27017/')
 db = client['licenta']
 collection = db['zones']
 
