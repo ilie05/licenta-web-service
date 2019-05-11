93647637b132b6bddb4a73b0377bb6bae04fe7d1
yoki <petras.moil@gmail.com>
2019-05-11 17:23:16 +0300

    first create zone file

--- service.py
+++ service.py
@@ -1,10 +1,47 @@
 import pymongo
+from bson.objectid import ObjectId
+import os
 
-client = pymongo.MongoClient('mongodb://192.168.56.255:27017/')
+serial_number = '2003080800'
+refresh = '2d'
+update_retry = '15m'
+expiry = '2w'
+ttl = '1h'
+
+client = pymongo.MongoClient('mongodb://192.168.56.1:27017/')
 db = client['licenta']
 collection = db['zones']
 
-print("Asd")
+# for doc in collection.find():
+#     print(doc)
 
-for doc in collection.find():
-    print(doc)
+object = collection.find_one({'_id': ObjectId("5cd5c0dbedb355c0a630e15a")})
+
+zone_file_path = '/var/named/{0}.zone'.format(object['domain'])
+
+# check if a zone file for this domain already exists
+if os.path.isfile(zone_file_path):
+    print("zone file already exists")
+    exit(-1)
+
+with open(zone_file_path, "w+") as zone_file:
+    zone_file.write('$ORIGIN ' + object['domain'] + '.' + '\n')   # $ORIGIN domain1.org.
+
+    if 'ttl' in object:                                           # $TTL 86400
+        ttl = object['ttl']
+
+    zone_file.write('$TTL ' + ttl + '\n')
+
+    zone_file.write('@\tIN\tSOA\tns1.' + object['domain'] + '.'
+                    + '\t' + object['admin_mail'] +
+                    '. (' + serial_number + refresh + update_retry
+                    + expiry + ttl + ')\n')
+
+    zone_file.write('\tIN\tNS\t' + 'ns1.' + object['domain'] + '.\n')
+    zone_file.write('\tIN\tMX\t' + object['mail_priority'] + '\t' + object['mail_host'] + object['domain'] + '.\n')
+    zone_file.write('\tIN\t' + object['ip_addr_type'] + '\t' + object['ip_address'] + '.\n')
+    zone_file.write('ns1\tIN\t' + object['ip_addr_type'] + '\t' + object['ip_address'] + '\n')
+    zone_file.write(object['mail_host'] + '\tIN\tA' + object['mail_ip_host'] + '\n')
+    zone_file.write(object['ip_host'] + '\tIN\t' + object['ip_addr_type'] + '\t' + object['ip_address'])
+
+
